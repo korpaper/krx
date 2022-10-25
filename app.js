b3a8e31962eb6657cfedfeb563e0df9ecc7be452
Hanji <hzhns2@gmail.com>
2022-10-25 21:18:11 +0900

    알고싶어요 시리즈

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.get('/search', function(req, res) {
     res.sendFile(__dirname + "/html/search.html");
 });
 
+// KRX 아카데미
 // 자본시장강의실 > 동영상강의 > 어린이 교실
 app.get('/child', function(req, res) {
     res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/video/child.html");
@@ -36,4 +37,14 @@ app.get('/youth', function(req, res) {
 // 자본시장강의실 > 동영상강의 > 초보투자자 교실
 app.get('/beginner', function(req, res) {
     res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/video/beginner.html");
+});
+
+// 자본시장강의실 > 책으로보는 > 자본시장 이해하기 시리즈
+app.get('/book1', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/book/book1.html");
+});
+
+// 자본시장강의실 > 책으로보는 > 알고싶어요 시리즈
+app.get('/book2', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/book/book2.html");
 });
