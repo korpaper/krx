1230eb13a7c332deb46adfda96501084e77ec71c
Hanji <hzhns2@gmail.com>
2022-10-27 17:29:08 +0900

    파생상품 다 함 휴

--- _krxuser/js/header.js
+++ _krxuser/js/header.js
@@ -15,29 +15,97 @@ $(document).ready(function(){
     $(document).on('mouseout','nav>ul>li',function(){
         $(this).children('.sub').css({'background':'none', 'color':'#000'});
     });
+    
+    $('.mb-nav > li:not(:first-of-type) .mb-depth2-wrap').slideUp();
     //헤더
-    var hjh1 ="";
-    var hjh2 ="";
+    var scUp ="";
+    var scDown ="";
     //스크롤 움직이면 뭔가 함
     $(window).scroll(function() {
-        //0.01초 뒤에 scrollTop 값을 1번 변수에 넣겠다
         setTimeout(function(){
-            hjh1 = $(window).scrollTop();
+            scUp = $(window).scrollTop();
         }, 10);
-        //0.02초 뒤에 scrollTop 값을 2번 변수에 넣겠다
         setTimeout(function(){
-            hjh2 = $(window).scrollTop();
+            scDown = $(window).scrollTop();
         }, 20);
 
         setTimeout(function(){
-        //1이 더 크면 보여주겠다(스크롤 올리는 상태)
-            if(hjh1 > hjh2){
+            if(scUp > scDown){
                 $(".pc-header").css('top', 0);
             }
-        //2가 더 크면 숨기겠다(스크롤 내리는 상태)
-            if(hjh1 < hjh2){
+            if(scUp < scDown){
                 $(".pc-header").css('top', '-140px');
             }
         }, 20);
     });
+
+    $('.menu-open').click(function(){
+        $('.mb-menu').css('display', 'block');
+    });
+    
+    $('.menu-close').click(function(){
+        $('.mb-menu').css('display', 'none');
+    });
+
+    let isActiveDepth = "krx";
+    $('.mb-nav>li>a').click(function(e){
+        const clickedValue = $(this).siblings().attr('class').split(' ')[1];
+        // if (clickedValue !== "deal" && isActiveDepth !== clickedValue) {
+        if (isActiveDepth !== clickedValue) {
+            $(`.mb-depth2-wrap.${clickedValue}`).slideDown(0);
+            $(`.mb-depth2-wrap.${clickedValue}`).css('z-index', '2');
+                $(`.mb-depth2-wrap.${isActiveDepth}`).slideUp(0);
+                isActiveDepth = clickedValue;
+        }
+        
+    })
+
+    // mb gnb
+    var $MGnbA = $('.mb-nav>li>a'),
+    $Mdep2A = $('.mb-depth2>li>a'),
+    $Mdep3A = $('.mb-depth3>li>a'),
+    $Mdep4A = $('.mb-depth4>li>a');
+
+    $MGnbA.on('click',function (e) {
+    if($(this).siblings('.mb-depth2').length > 0){
+    }
+    if(!$(this).parent().hasClass('active3')){
+        $(this).parent().addClass('active3').siblings().removeClass('active3');
+    }
+    });
+
+    $Mdep2A.on('click',function (e) {
+    if($(this).siblings('.mb-depth3').toggle().length > 0){
+    }
+
+    $(this).find('.mb-depth3>li>a').stop().slideUp().toggle();
+    if(!$(this).parent().hasClass('active4')){
+        $(this).parent().addClass('active4').siblings().removeClass('active4').find('.mb-depth3').stop().slideUp();
+        $(this).siblings().stop().slideDown();
+    }
+    });
+
+    $Mdep3A.on('click',function (e) {
+        if($(this).siblings('.mb-depth4').toggle().length > 0){
+        }
+    
+        $(this).find('.mb-depth4>li>a').stop().slideUp().toggle();
+        if(!$(this).parent().hasClass('active5-1')){
+            $(this).parent().addClass('active5-1').siblings().removeClass('active5-1').find('.mb-depth4').stop().slideUp();
+            $(this).siblings().stop().slideDown();
+        }
+        });
+    
+    $Mdep4A.on('click',function (e) {
+        if($(this).siblings('.dep4').length > 0){
+        }
+    
+        $(this).find('.mb-depth4>li>a').stop().slideUp();
+        if(!$(this).parent().hasClass('active5')){
+            $(this).parent().addClass('active5').siblings().removeClass('active5');
+            $(this).siblings();
+        }
+        });
+
 })
+
--- app.js
+++ app.js
@@ -48,3 +48,53 @@ app.get('/book1', function(req, res) {
 app.get('/book2', function(req, res) {
     res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/book/book2.html");
 });
+
+// 자본시장강의실 > 신나는 경제게임
+app.get('/game', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/game.html");
+});
+
+// 자본시장강의실 > 재미있는 증권 애니메이션 > 황비와 웅비의 증권 테마파크
+app.get('/ani1', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/ani/thema-park.html");
+});
+
+// 자본시장강의실 > 재미있는 증권 애니메이션 > 흰수염 할아버지의 장난감 가게
+app.get('/ani2', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/ani/toy-store.html");
+});
+
+// 자본시장강의실 > 재미있는 증권 애니메이션 > 신나는 증권 선물교실
+app.get('/ani3', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/capital/ani/fun-class.html");
+});
+
+// 파생상품강의실 > 강의안내
+app.get('/det-guide', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/derivative/det-guide.html");
+});
+
+// 파생상품강의실 > 파생상품의 이해
+app.get('/about-det', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/derivative/about-det.html");
+});
+
+// 파생상품강의실 > 파생상품거래제도의 이해
+app.get('/about-deal', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/derivative/about-deal.html");
+});
+
+// 파생상품강의실 > 파생상품거래의 위험성
+app.get('/det-risk', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/derivative/det-risk.html");
+});
+
+// 파생상품강의실 > 알기쉬운 파생상품
+app.get('/easytoknow', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/derivative/easytoknow.html");
+});
+
+// 환리스크관리 강의실 > 환리스크 관리의 기본개념
+app.get('/basic', function(req, res) {
+    res.sendFile(__dirname + "/html/finance-edu/krx-academy/risk/basic.html");
+});
